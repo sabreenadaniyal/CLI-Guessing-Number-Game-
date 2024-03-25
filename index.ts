2f5cb2575093b291ede28f36398a809b671bda1c
Sabreena Daniyal <160657530+sabreenadaniyal@users.noreply.github.com>
2024-03-26 03:34:10 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+console.log("Welcome to Number Guessing Game");
+
+const answer = await inquirer.prompt([
+    { name: "userGuessedNumber", type: "number", message: "Please guess a number between 1-6", },
+]);
+
+//conditional statement
+
+if (answer.userGuessedNumber === randomNumber) {
+    console.log(chalk.yellowBright("CONGRATULATIONS! you Win"));
+}
+else {
+    console.log(chalk.red("Sorry! you loose"));
+}
+
+
